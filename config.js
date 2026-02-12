680f71b499819936d0bbdbfcdf00bacdc09adf72
navs7 <navs7@users.noreply.github.com>
2026-02-12 17:30:06 +0530

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwpedpqUrDzfVHA3z2WWcn1lNTS9i0dI5OsFlBwf0iwjGqVQykACqJNMs1UhIuh1rzL/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbxcBIijv5aB6zkriXmTXAaMRuyBsVv8GURnh3gXOgQuz68EKP3aqU1lTyeyiz_rowiu/exec"
 };
+
